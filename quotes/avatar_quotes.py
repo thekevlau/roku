262a30e800a3c5b71577af176f543a08c7b494a2
Kevin Lau <thekevlau@gmail.com>
2016-12-02 21:18:14 -0500

    refactored quotes

---
+++ quotes/avatar_quotes.py
@@ -0,0 +1,74 @@
+avatar_quotes = [
+    # Laghima, The Legend of Korra
+    "Instinct is lie, told by a fearful body, hoping to be wrong.",
+
+    # Zaheer, The Legend of Korra
+    "Let go your earthly tether. Enter the void. Empty, and become wind.",
+
+    # Iroh, Avatar the Last Airbender
+    "Sometimes life is like this dark tunnel- you can't always see the light at the end of the tunnel. But if you just keep moving, you will come to a better place.",
+    "It is important to draw wisdom from different places. If you take it from only one place it will become rigid and stale.",
+    "In the darkest times, hope is something you give yourself. That is the meaning of inner strength.",
+    "Who knocks at the garden gate?",
+    "The best tea tastes delicious whether it comes in a porcelain pot or a tin cup.",
+    "Its time for you to look inward and begin asking yourself the big questions: Who are you? and What do you want?",
+    "Perfection and power are overrated. I think you are very wise to choose happiness and love. There are reasons each of us are born. we have to find those reasons.",
+    "Pride is not the opposite of shame, but its source. True humility is the only antidote to shame.",
+    "There's nothing wrong with a life of peace and prosperity. I suggest you think about waht it is that you want from our life, and why.",
+    "Leaves from the vine, falling so slow...like fragile, tiny shells drifting in the foam. Little soldier boy...come marching home. Brave soldier boy...comes marching home.",
+    "You're looking at the rare white dragon bush. Its leaves make a tea so delicious it's heartbreaking!....That, or its the white jade bush, which is poisonous.",
+    "I always tried to tell you that Pai Sho is more than just a game.",
+    "Life happens wherever you are, whether you make it or not.",
+    "Euch! This tea is nothing more than hot leaf juice!",
+    "Sick of tea? That's like being sick of breathing!",
+    "It is usually best to admit mistakes when they occur, and to seek to restore honor............But not this time. Run!",
+    "While it is always best to believe in one self, a little help from others can be a great blessing.",
+    "I was never angry with you. I was sad, because I was afraid you'd lost your way.",
+    "Yes, you have. You struggled. You suffered. But you have always followed your own path. You've restored your own honour.",
+    "You know, Prince Zuko, destiny is a funny thing. You never know how things are going to work out. But if you keep an open mind and an open heart, I promise you will find your own destiny someday.",
+    "Are you so busy fighting you cannot see your own ship has set sail?",
+    "You sound like my nephew. Always thinking you need to do things on our own without anyone's support.",
+    "There is nothing wrong with letting people who love you, help you. Not that I love you. I just met you.",
+    "I know you're not supposed to cry over spilled tea, but...it's just so sad!",
+    "Who would have thought that, after all these years, I would return to the scene of my greatest military disgrace....as a tourist!",
+    "There is energy all around us. The energy is both yin and yang - positive energy and a negative energy. Only a select few Firebenders can separate these energies. This creates an imbalance. The energy wants to restore balance, and in the moment the energies come crashing back together, you provide release and guideance- creating lightning.",
+    "Is this your own destiny? Or is it a destiny someone else has tried to force on you?",
+    "Sometimes the best way to solve your own problems is to help someone else.",
+    "Even in the material world, you will find that if you look for the light, you can often find it. But if you look for the dark, that is all you will ever see.",
+    "Many things that seem threatening in the dark, become welcoming when we shine light on them.",
+
+    # Pian Dao, Avatar the Last Airbender
+    "Creativity, versatility, intelligence. These are the traits that define a great (swords)man.",
+
+    # Avatar Roku, Avatar the Last Airbender
+    "It was bitter work, but the results...were worth it.",
+
+    # Jeong Jeong, Avatar the Last Airbender
+    "Learn restraint or risk destroying yourself and everything you love.",
+
+    # Lion Turtle
+    "The true mind can weather all the lies and illusions without being lost. The true heart can touch the poison of hatred without being harmed. Since beginningless time, darkness thrives in the void, but always yields to purifying light.",
+
+    # Aang?
+    "Revenge is like a two-headed rat-viper. While you watch your enemy go down, you're being poisoned yourself.",
+    "If you want to be a bender, you have to let go of fear.",
+    "When we hit our lowest point, we are open to the greatest change.",
+
+    # Huu, the swamp man
+    "You think you're any different from me, or your friends, or this tree? If you listen hard enough, you can hear every living thing breathing together. You can feel everything growing. We're all living together, even if most folks don't act like it. We all have the same roots, and we are all branches of the same tree.",
+
+    # Ursa
+    "No matter how things seem to change. Never forget who you are.",
+
+    # Ozai
+    "You will learn respect. And suffering will be your teacher.",
+
+    # Koh
+    "Tui and La, the moon and ocean, have always circled each other in an eternal dance. They balance each other. Push and Pull. Life and Death. Good and Evil. Yin and Yang.",
+
+    # Guru Pathik
+    "The greatest illusion of this world is the illusion of separation. Things you think are separate and different are actually one and the same. We are all one people, but we live as if divided.",
+
+    # Suki
+    "I am a warrior, but I'm a girl too.",
+]
---
+++ quotes/sandy_quotes.py
@@ -0,0 +1,6 @@
+quotes_for_sandy = [
+  "Love is here.",
+  "Protecting what has been won, clinging to nothing.",
+  "We are not only what you've become; we are what you've already become.",
+  "Wash the dishes, to wash the dishes.",
+]
