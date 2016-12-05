1f2e0516d306d22c6711ccd784164fa31b1b9b53
Kevin Lau <thekevlau@gmail.com>
2016-12-05 02:09:06 -0500

    added more quotes

--- quotes/kevin_quotes.py
+++ quotes/kevin_quotes.py
@@ -1,8 +1,15 @@
 kevin_quotes = [
-    # High Level Coach Style Reminders and Feedback- My Current State and Goals
+    # My Current State and Goals
+    # High Level Coach Style Reminders and Feedback
     "You are putting off the Art of War in order to focus on school right now. Focus on crushing it.",
+    "You have two main focuses right now: Train Discipline, Train Vulnerability.",
     "School comes first. Then comes job seeking. Studying the interesting stuff-- finance, badminton, strategy is the reward to follow.",
     "The real work only begins after your brain and body are screaming that they are at their limits. Your real max is 3x where you think it is.",
+    "In order to change, you must do differently, even if it seems like everything is going well, otherwise you will stagnate. But doing something differently, means you will fail. Fail first, to come back stronger, able to reach higher than ever before.",
+    "You have to work your fucking face off, to kill it. There is no substitute. And the only way you'll work 15 hours a day and pay that sacrifice to be away from loved ones, to travel and hustle is if you're doing something around the subject matter that you live, breathe and would die for.",
+    "We don't get what we want in life-- we get what we have to have.",
+    "Swimmers, they have these daily rituals exercises they do before practice. Michael Phelps, he did 3 or 4 of regular guys' rituals every day.",
+    "You become what you think about all day long. Be careful about what you think about-- don't think about anything you don't want to manifest in your life",
 
     # Things I wish to always be at the top of my conciousness
     # Things I want to repeat to myself over and over again so that I don't forget.
@@ -11,6 +18,8 @@ kevin_quotes = [
     "The ultimate goal is intimidating, overwhelmingly dominant work ethic.",
     "Fear of vulnerability and humiliation-- ego -- is your biggest bottleneck.",
     "I want a work ethic that can strike fear into the hearts of enemies. A reputation.",
+    "It isn't about 'ok I'll let myself prove that I am these things before I believe I'm these things.' You have to BELIEVE it first, based on no evidence, you have to take that leap of faith first, before you will achieve it.",
+    "Ask yourself-- are you uncomfortable right now?. If the answer is yes, you're exactly where you need to be right now.",
     # better than this, is the confidence that you can accomplish anything.
 ]
 
