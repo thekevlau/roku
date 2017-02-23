1bd6a7b68d4e9a638197a763ff6bbe1128cac8a1
Kevin Lau <thekevlau@gmail.com>
2017-02-23 16:49:36 -0500

    string formatting update

--- quotes/general_motivational_quotes.py
+++ quotes/general_motivational_quotes.py
@@ -4,66 +4,68 @@ general_motivational_quotes = []
 
 # quotes contributed to the public quote library by Kevin
 kevin_quotes = [
-    ## Reminders for Key Life Lessons
+    # Reminders for Key Life Lessons
     # My Brain
-    """It is important to regularly stop, re-orient and re-focus on one's
-        goals. This is how one achieves long term focus.""",
+    ("It is important to regularly stop, re-orient and re-focus on one's"
+        " goals. This is how one achieves long term focus."),
     "Discipline opens the doors to infinite possibilities. Stay sharp.",
     "You can learn anything. You were built for it!",
     "Devote yourself to the perfection of today's tasks.",
     "The ability to focus is what differentiates the top 1%",
-    """You don't rise to the level of your hopes, you fall to the level your
-        training""",
-    """Always be the weakest person in the room and focus on sponging from
-        everyone else. Revel in your failure as it is the symptom
-        of growth.""",
-    """To be able to endure the pain of being a repeated failure is not
-        strength. To be strong is when you are able to achieve success once
-        again. To stand back up after repeated failure and achieve success
-        once again.""",
+    ("You don't rise to the level of your hopes, you fall to the level your"
+        " training"),
+    ("Always be the weakest person in the room and focus on sponging from"
+        " everyone else. Revel in your failure as it is the symptom of"
+        " growth."),
+    ("To be able to endure the pain of being a repeated failure is not"
+        " strength. To be strong is when you are able to achieve success once"
+        " again. To stand back up after repeated failure and achieve success"
+        " once again."),
     "Talent x Effort = Skill. Skill x Effort = Achievement.",
-    """If it seems like you're doing everything right and you're killing the
-        game all the time, you aren't pushing yourself hard enough.""",
+    ("If it seems like you're doing everything right and you're killing the"
+        " game all the time, you aren't pushing yourself hard enough."),
     "It takes 120% to reach 100%",
-    """Success is 99% HARD WORK. Sit down. Shut up. Suck it up and get back
-        to work.""",
+    ("Success is 99% HARD WORK. Sit down. Shut up. Suck it up and get back"
+        " to work."),
     "Remember your basics! They are your greatest weapons.",
 
     # Steen Pedersen
-    """Accept that it's a natural feeling that you're nervous. Don't be
-        afraid of it and re-focus on the task at hand. If you're all the time
-        monitoring, questioning and second guessing yourself, instead of
-        trying to do what you've practiced, you'll make it harder for
-        yourself.""",
+    ("Accept that it's a natural feeling that you're nervous. Don't be"
+        " afraid of it and re-focus on the task at hand. If you're all the"
+        " time monitoring, questioning and second guessing yourself, instead"
+        " of trying to do what you've practiced, you'll make it harder for"
+        " yourself."),
 
     # Motivation Chrome Extension
-    """If you don't like something, change it. If you can't change it, change
-        your attitude. Don't complain.""",
+    ("If you don't like something, change it. If you can't change it, change"
+        " your attitude. Don't complain."),
 
     # Bruce Lee
-    """I'm not in this world to live up to your expectations and you're not in this world to live up to mine.""",
+    ("I'm not in this world to live up to your expectations and you're not in"
+        " this world to live up to mine."),
 
     # Book of Five Rings, I think
-    """Be determined through calm-- meet situations without tenseness, yet
-        not recklessly. Your spirited settled, yet unbiased.""",
-
+    ("Be determined through calm-- meet situations without tenseness, yet"
+        " not recklessly. Your spirited settled, yet unbiased."),
 
     # Tim Ferris Podcast
-    "You must be an A+ in order to accomplish a C-level, 5 year plan entirely.",
+    ("You must be an A+ in order to accomplish a C-level, 5 year plan"
+        " entirely."),
+
     # Haikyuu
     "In order to enjoy competition, you must become strong.",
-    """There is no battle you cannot win, but also no battle you will win for
-        sure.""",
+    ("There is no battle you cannot win, but also no battle you will win for"
+        " sure."),
 
     "Write your own narrative. Believe it, live it and it will become true.",
 
     # Fearless
-    """The true purpose of competition is to better understand yourself,
-        because your only true opponent in life is yourself.""",
+    ("The true purpose of competition is to better understand yourself,"
+        " because your only true opponent in life is yourself."),
 
     # Peter Buhr
-    """Know your craft. There is no excuse-- you should know everything there
-        is to know and then some, about your craft.""",
+    ("Know your craft. There is no excuse-- you should know everything there"
+        " is to know and then some, about your craft."),
 
     # Quote by Stephen McCranie, sourced from karenx.com
     "The master has failed more times than the beginner has even tried",
@@ -71,8 +73,8 @@ kevin_quotes = [
     ## Motivation
     "Be better than you were yesterday.",
     "110%. Every day. Exceed your limits and grow-- Plus Ultra.",
-    """It's about how much you can get hit, and still get back up and keep
-        going.""",
+    ("It's about how much you can get hit, and still get back up and keep"
+        " going."),
     "Good effort today. Do better tomorrow.",
     # Alphalete Video
     "Proud, but never Satisfied.",
