e08971a0a91971be3265d6a34658c0a760fd49ca
Kevin Lau <thekevlau@gmail.com>
2017-04-10 11:38:31 -0400

    wrote a small script to send message to all users

---
+++ send_sms_to_all.py
@@ -0,0 +1,28 @@
+# send message to all users
+
+from twilio.rest import TwilioRestClient
+
+import twilio_creds
+import phone_numbers
+
+
+client = TwilioRestClient(account=twilio_creds.twilio_account,
+                          token=twilio_creds.twilio_token)
+
+# phone_numbers = phone_numbers.phone_numbers
+
+
+def send_sms_to_all(original_message):
+    for name, phone in phone_numbers.phone_numbers.iteritems():
+        salutation = "Hello %s! " % (name.title())
+        message = salutation + original_message
+        print("sending the following message to %s at %s: %s" % (name, phone,  message))
+        client.messages.create(
+            # TODO: replace with\ messaging service SID later
+            from_="(647) 560-9023",
+            to=phone,
+            body=message
+        )
+
+
+send_sms_to_all("If you hadn't already noticed, you haven't been getting quotes on your phone for quite a while now! Sorry about that-- it took a while to sort out some payment issues with Twilio, but we will be back to our regularly (randomly) scheduled programming starting today! Hope you've been enjoying the Project Roku beta otherwise :)")
