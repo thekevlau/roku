37e367ba2a24f8bd27d2d6104e8d5ebb04c04d1e
Sandy Wu <s@stripe.com>
2017-04-14 13:53:23 -0700

    Condense Sandy quote to koan

--- quotes/sandy_quotes.py
+++ quotes/sandy_quotes.py
@@ -2,52 +2,5 @@
 # Personal Quotes are only sent to the specific individual
 
 sandy_quotes = [
-    "Love is here.",
-    "Protecting what has been won, clinging to nothing.",
-    "We are not only what you're becoming; we are what you've already become.",
-    "Wash the dishes to wash the dishes.",
-    "Whatever arises love that",
-    ("Absolute freedom itself is necessary before you can acquire absolute"
-        " freedom"),
-    "You think that what you do not do yourself does not happen",
-    "Free.",
-    "I accept.",
-    "Observe the mind just as it is",
-    "Try to be less, not more",
-    "All things that arise are natured to pass",
-    "The knowing is not changed or altered by what is being known",
-    "Rest in the unarisen",
-    ("At all times, simply rely on a joyful mind. Don't expect a standing"
-        " ovation"),
-    ("It is the process of change that becomes the object of contemplation."
-        " Not what is arising, but that it is"),
-    ("Notice the quality of the mind. Is it noticing the arising? The"
-        " passing? Both the arising and the passing?"),
-    "Noticing the arising, presence, and passing away of all phenomena.",
-    ("Mindfully contemplating speech. Timely or not timely. True or not true."
-        " Gentle or harsh. Meant to do good or harm. Spoken from a place of"
-        " loving kindness or cruelty"),
-    "Mindfully contemplating the body internally and externally",
-    ("Our expectations of others are simply reminding us of the gifts that we"
-        " are here to offer others in every moment"),
-    ("Let go of even the desire to be in the next moment. And even the desire"
-        " to not be"),
-    "Nothing to do, nothing to be, nothing to have",
-    "No need to seek out things to notice. Just notice whatever you do notice",
-    "Movement disguises dukha",
-    "Mara I see you",
-    "Simple and ease [should be the way of your practice]",
-    "Today's enlightenment is tomorrow's mistake",
-    ("The way that you know if you're meditating in action is to see if you"
-        " can stop on a dime any time you want. You should be able to go, at"
-        " any time, into an absolutely stable, motionless state without"
-        " struggle if you're really 'meditating in daily life.'"),
-    ("If you're not okay with the current moment lasting forever, then Stop"
-        " and look more closely."),
-    ("Only one moment exists, and only thing to do is to apply 'the"
-        " technique' to an arising in the Now. (All arisings can be"
-        " deconstructed into the Now. To practice, there is only one thing to"
-        " do. Apply any technique of your choosing, once, to the current"
-        " moment. That is all. No need to beat yourself up over not having"
-        " practice correctly or wasted time/opprtunities!)"),
+    "I am not in the world. The world is in me",
 ]
