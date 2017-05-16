02dac52e9de4a26726d49077e4528ccb23b7d9a7
Kevin Lau <thekevlau@gmail.com>
2017-05-16 10:03:38 -0400

    deprecating send_sms_to_sandy.py, really could just delete it.

--- send_sms_to_sandy.py
+++ send_sms_to_sandy.py
@@ -1,3 +1,4 @@
+# DEPRICATED
 import random
 
 from twilio.rest import TwilioRestClient
@@ -15,7 +16,7 @@ sandy_quotes = sandy_quotes.sandy_quotes
 # note: cron job runs once per minute
 # send ~30 messages between 7am and 12pm PST, so 17 hours or 1020 minutes
 for i in xrange(100000):
-    if not (random.random() <= (30 / 1020000000.0)):
+    if not (random.random() <= (1 / 3400000.0)):
         print("not this time")
         continue
 
