f49e8e0cd963d5a759e76567692c4d7d8aeafafe
Kevin Lau <thekevlau@gmail.com>
2017-05-17 10:16:36 -0400

    fixed mixed spaces and tabs

--- quotes/andee_quotes.py
+++ quotes/andee_quotes.py
@@ -3,12 +3,12 @@
 
 andee_quotes = [
     ("Whatever you exercise gets stronger. Build concentration, build the"
-    	" ability to resist distraction. It will eventually become easier."),
+        " ability to resist distraction. It will eventually become easier."),
     "Never put off to tomorrow what you can do today.",
     ("Concentration can be cultivated. One can learn to exercise willpower,"
-    	" discipline one's body and train one's mind."),
+        " discipline one's body and train one's mind."),
     "Concentration is the root of all the higher abilities of man.",
     "Doubt kills more dreams than failure ever will.",
     ("You generally feel better after doing the thing that involves"
-    	" human contact and going out. Go out and talk to people!")
+        " human contact and going out. Go out and talk to people!")
 ]
